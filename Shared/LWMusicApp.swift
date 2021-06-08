457c9cb63e8485b06bcdd855fd08a16bb5baa554
wulin03 <wulin03@kuaishou.com>
2021-06-08 14:45:27 +0800

    Initial Commit

---
+++ Shared/LWMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  LWMusicApp.swift
+//  Shared
+//
+//  Created by vvusu on 6/8/21.
+//
+
+import SwiftUI
+
+@main
+struct LWMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
